ee085fe7268310ef203e3e55dacbd014e6d24efc
Ataias Pereira Reis <ataiasreis@gmail.com>
2016-02-06 09:42:06 -0200

    AsyncTask was not being used correctly I was just creating an object as a property of the ForecastActivity class and calling doInBackground, but I should create a new one when the button is clicked and call .execute

--- app/src/main/java/io/github/ataias/othersunshine/ForecastActivity.java
+++ app/src/main/java/io/github/ataias/othersunshine/ForecastActivity.java
@@ -1,11 +1,14 @@
 package io.github.ataias.othersunshine;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -18,17 +21,20 @@ import java.net.URL;
 
 public class ForecastActivity extends AppCompatActivity {
 
+    private final String LOG_TAG = ForecastActivity.class.getSimpleName();
+
     ArrayAdapter<String> mItemsAdapter;
+    String[] mForecast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.i(LOG_TAG, "Criou o fetchWeatherTask");
 
 //        String[] params = {"http://api.openweathermap.org/data/2.5/forecast/daily?q=Brasilia&mode=json&units=metric&cnt=7&appid=0614cde9f06e70606dca2278f05f6641"};
 
-        String[] forecast = {
+        mForecast = new String[] {
                 "Today",  "Tomorrow", "Day after tomorrow", "Hey, it is 88 degress",
                 "Hello!", "Hello world!", "E aí? Beleza?"};
 
@@ -40,7 +46,7 @@ public class ForecastActivity extends AppCompatActivity {
                 R.layout.list_view_item,
                 //ID of the TextView
                 R.id.list_item,
-                forecast);
+                mForecast);
 
         //Container is the parent view of the fragment
         //If not used, it complains that it is a static method in a non-static environment
@@ -65,7 +71,8 @@ public class ForecastActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_refresh) {
+            new FetchWeatherTask().execute();
             return true;
         }
 
