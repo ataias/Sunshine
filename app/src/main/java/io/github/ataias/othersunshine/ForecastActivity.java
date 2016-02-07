55c3f28e98cf7d9ff7444de828a9df603b347df8
Ataias Pereira Reis <ataiasreis@gmail.com>
2016-02-07 10:31:11 -0200

    Added implicit intent to open map application to the user location

--- app/src/main/java/io/github/ataias/othersunshine/ForecastActivity.java
+++ app/src/main/java/io/github/ataias/othersunshine/ForecastActivity.java
@@ -1,8 +1,10 @@
 package io.github.ataias.othersunshine;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +14,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -83,7 +86,13 @@ public class ForecastActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             Intent settingsActivityIntent = new Intent(getApplicationContext(), SettingsActivity.class);
             startActivity(settingsActivityIntent);
+            return true;
         }
+
+        if(id == R.id.action_map) {
+            showMap();
+        }
+
 
         return super.onOptionsItemSelected(item);
     }
@@ -101,5 +110,26 @@ public class ForecastActivity extends AppCompatActivity {
         String unit = prefs.getString(getString(R.string.pref_temperature_unit_key), possibleUnits[0]);
 
         new FetchWeatherTask().execute(city, unit);
+    }
+
+    private void showMap() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = prefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_default_location));
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:0,0?q=" + location));
+        boolean mapAppFound = intent.resolveActivity(getPackageManager()) != null;
+        if (mapAppFound) {
+            startActivity(intent);
+        } else {
+            Context context = getApplicationContext();
+            CharSequence text = "Map application not found";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+
+
     }
 }
